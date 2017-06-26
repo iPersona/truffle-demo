24ebbbb8bb7546a854cd29d5216a368abd9547b2
linw <linw@landicorp.com>
2017-06-26 17:39:54 +0800

    test(test): add advance function test

--- contracts/AdvanceFunctionDemo.sol
+++ contracts/AdvanceFunctionDemo.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.4.0;
 
 
-contract AdvanceFunctionDemo {
-    function AdvanceFunctionDemo(){
+contract A {
+    function A(){
 
     }
     function f() internal returns (uint) {
         return 1;
     }
 
-    function callInternally(){
-        f();
+    function callInternally() returns (uint){
+        return f();
     }
 
     //以`external`的方式调用
@@ -21,3 +21,16 @@ contract AdvanceFunctionDemo {
     }
 }
 
+contract B {
+    uint value;
+
+    //以`external`的方式调用另一合约中的函数
+    function callExternal(A a) {
+//        value = a.f();    // 声明为 internal 的函数，只能在合约内部调用，外部无法调用
+        value = a.callInternally();
+    }
+
+    function getValue() returns (uint) {
+        return value;
+    }
+}
--- test/TestAdvanceFunctionDemo.sol
+++ test/TestAdvanceFunctionDemo.sol
@@ -6,6 +6,11 @@ import "../contracts/AdvanceFunctionDemo.sol";
 
 contract TestAdvanceFunctionDemo {
     function testF() {
-
+        A a = A(DeployedAddresses.A());
+        B b = B(DeployedAddresses.B());
+        var valueExp = 1;
+        b.callExternal(a);
+        var valueAct = b.getValue();
+        Assert.equal(valueExp, valueAct, "value not equal!");
     }
 }
